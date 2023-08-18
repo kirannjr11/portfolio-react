5b7901065da4645f6a9870ea665f5ac5053ff775
unknown <ghalekiran03@gmail.com>
2023-08-19 00:04:45 +0900

    photo added

--- src/coponents/Productlist/ProductList.jsx
+++ src/coponents/Productlist/ProductList.jsx
@@ -7,7 +7,7 @@ const ProductList= ()=>{
             <div className="pl-texts">
                 <h1 className="pl-title"> Create & Inspire</h1>
                 <p className="pl-desc">
-                My proficiency extends beyond mere code; I pride myself on my ability to empathize with users and tailor my creations to meet their needs seamlessly. Collaboration is second nature to me, as I relish the opportunity to work in multidisciplinary teams, where my skills harmoniously merge with others' to create exceptional results. Detail-oriented and dedicated, I approach every project with a meticulous eye, ensuring that even the smallest elements contribute to a cohesive and polished final product.
+                    
                 </p>
             </div>
 
--- src/data.js
+++ src/data.js
@@ -1,26 +1,26 @@
 export const Products = [
     {
         id:1,
-        img:"https://cdn.pixabay.com/photo/2018/05/04/20/01/website-3374825_1280.jpg",
-        link:"http://google.com",
+        img:"./images/weather.png",
+        link:"https://weather.kiranghale.com/",
 
     },
     {
         id:2,
-        img:"https://media.istockphoto.com/id/1371339413/photo/co-working-team-meeting-concept-businessman-using-smart-phone-and-digital-tablet-and-laptop.webp?b=1&s=612x612&w=0&k=20&c=5f5qgmdm1_isNCz_ISP61-2QPfEtRl0Fz1ZVplTD7ek=",
-        link:"http://google.com",
+        img:"./images/quickchat.png",
+        link:"https://quickchat.kiranghale.com/",
 
     },
     {
         id:3,
-        img:"https://media.istockphoto.com/id/1301200293/photo/woman-outside-in-city-checking-weather-forecast-on-her-mobile-phone.webp?b=1&s=612x612&w=0&k=20&c=l306oN7n8YdeMtRzLw0ps2uzJC2JG9lsnBSD2sKO-Uw=",
-        link:"http://google.com",
+        img:"./images/portfolio.png",
+        link:"http://portfolio.kiranghale.com",
 
     },
     {
         id:4,
-        img:"https://cdn.pixabay.com/photo/2016/12/11/21/37/whatsapp-1900453_640.png",
-        link:"http://google.com",
+        img:"./images/calculator.png",
+        link:"http://calculator.kiranghale.com",
 
     },
     {
